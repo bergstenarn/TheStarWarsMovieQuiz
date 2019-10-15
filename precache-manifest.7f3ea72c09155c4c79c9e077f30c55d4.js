403762406865aadb582e5f723f29b483771bcede
bergstenarn <andersbergsten@yahoo.se>
2019-10-15 19:56:23 +0300

    Updates

---
+++ precache-manifest.7f3ea72c09155c4c79c9e077f30c55d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301477534b3b6206e4709063b4c686d2",
+    "url": "/TheStarWarsMovieQuiz/index.html"
+  },
+  {
+    "revision": "f0f3c6d12fd381576889",
+    "url": "/TheStarWarsMovieQuiz/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3a92569f0276f9fd4108",
+    "url": "/TheStarWarsMovieQuiz/static/css/main.2c699dcd.chunk.css"
+  },
+  {
+    "revision": "f0f3c6d12fd381576889",
+    "url": "/TheStarWarsMovieQuiz/static/js/2.45be4b56.chunk.js"
+  },
+  {
+    "revision": "3a92569f0276f9fd4108",
+    "url": "/TheStarWarsMovieQuiz/static/js/main.3aa68bb7.chunk.js"
+  },
+  {
+    "revision": "c45ff600e6bbe2681ea4",
+    "url": "/TheStarWarsMovieQuiz/static/js/runtime-main.23008591.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/TheStarWarsMovieQuiz/static/media/SEGA.12a3db8b.woff"
+  }
+]);
